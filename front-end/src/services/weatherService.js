026d2a2cfc2e84f1f10a0cd43ebd677089cea669
Basudeb Bej <basudebbej2025@gmail.com>
2026-03-29 14:57:51 +0530

    done

--- front-end/src/services/weatherService.js
+++ front-end/src/services/weatherService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBaseUrl =
-  import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") ??
+  import.meta.env.VITE_API_URL?.replace(/\/$/, "") ??
   (import.meta.env.DEV ? "http://localhost:8000" : "");
 
 function getApiUrl(path) {
